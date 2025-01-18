81024237d0ba5e17b10eaadd92f4927d3ef14dc5
evertjhernandez <evert.hernandez@pixelechoes.io>
2025-01-18 15:10:29 -0500

    initial push

---
+++ PyReflexCounterExample/PyReflexCounterExample.py
@@ -0,0 +1,35 @@
+import reflex as rx
+from rxconfig import config
+
+class State(rx.State):
+    count: int = 0
+
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
+
+def index():
+    return rx.flex(
+        rx.hstack(
+            rx.button(
+                "Decrement",
+                color_scheme="ruby",
+                on_click=State.decrement,
+            ),
+            rx.heading(State.count, font_size="2em"),
+            rx.button(
+                "Increment",
+                color_scheme="grass",
+                on_click=State.increment,
+            ),
+            spacing="4",
+        ),
+        justify="center",
+        align="center",
+        height="100vh"
+    )
+
+app = rx.App()
+app.add_page(index)
